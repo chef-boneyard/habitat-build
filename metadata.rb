cf40e6ba11f5c872ed0afc081594556a9c9e535a
jtimberman <joshua@chef.io>
2016-05-31 14:21:49 -0600

    dont use delivery truck. not appropos here

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,10 @@ name 'habitat-build'
 maintainer 'The Habitat Maintainers'
 maintainer_email 'humans@habitat.sh'
 license 'apache2'
-version '0.5.11'
+version '0.5.12'
 
 depends 'delivery-sugar'
 depends 'delivery_build'
-depends 'delivery-truck'
 
 issues_url 'https://github.com/habitat-sh/habitat-build-cookbook/issues'
 source_url 'https://github.com/habitat-sh/habitat-build-cookbook'
