afe1fa65f939bf797d5716193404d9a55d4bb043
Seth Chisamore <schisamo@chef.io>
2017-03-27 11:21:10 -0400

    Save build version data using Delivery Sugar DSL
    We should not re-invent the wheel here.
    
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- resources/hab_build.rb
+++ resources/hab_build.rb
@@ -17,25 +17,6 @@ property :live_stream, [TrueClass, FalseClass], default: true
 
 action_class do
   include HabitatBuildCookbook::Helpers
-
-  def data_bag_item_id
-    build_version.tr('/', '-')
-  end
-
-  # The current acceptance environment on the server
-  def existing_acceptance_environment
-    Chef::ServerAPI.new.get("environments/#{get_acceptance_environment}")
-  rescue Net::HTTPServerException => e
-    raise e unless e.response.code.to_i == 404
-    {}
-  end
-
-  def updated_environment_overrides
-    existing_overrides = existing_acceptance_environment.fetch('override_attributes', {})
-    existing_overrides['applications'] ||= {}
-    existing_overrides['applications'][new_resource.name] = build_version
-    existing_overrides
-  end
 end
 
 action :build do
@@ -43,9 +24,11 @@ action :build do
     command "sudo -E #{hab_binary} studio" \
             " -r #{hab_studio_path}" \
             " build #{plan_dir}"
-    environment('TERM' => 'vt100',
-                'HAB_ORIGIN' => origin,
-                'HAB_NONINTERACTIVE' => 'true')
+    environment(
+      'TERM' => 'vt100',
+      'HAB_ORIGIN' => origin,
+      'HAB_NONINTERACTIVE' => 'true'
+    )
     cwd new_resource.cwd
     live_stream new_resource.live_stream
   end
@@ -54,38 +37,25 @@ end
 action :publish do
   execute 'upload-pkg' do
     command lazy { "#{hab_binary} pkg upload --url #{url} #{hab_studio_path}/src/results/#{artifact}" }
-    env('HOME' => home_dir,
-        'HAB_AUTH_TOKEN' => auth_token,
-        'HAB_NONINTERACTIVE' => 'true')
+    env(
+      'HOME' => home_dir,
+      'HAB_AUTH_TOKEN' => auth_token,
+      'HAB_NONINTERACTIVE' => 'true'
+    )
     live_stream new_resource.live_stream
     sensitive true
   end
 
-  load_delivery_chef_config
-  chef_data_bag new_resource.name
-
-  # We need to be clear here to ensure that the magic done by cheffish
-  # doesn't cause any unexpected data bag item naming or contents.
-  # Thus, we have a unique project name for the resource, which does
-  # not get used because we're setting the `id` and the `data_bag`
-  # properties. We need to use `lazy {}` on the `id` and `raw_data`
-  # because those contain calculated values from a previously
-  # converged resource, `hab_build`, above.
-  #
-  chef_data_bag_item "store-#{new_resource.name}-artifact-data" do
-    id lazy { data_bag_item_id }
-    data_bag new_resource.name
-    raw_data lazy {
-      { 'id' => data_bag_item_id,
-        'version' => build_version,
+  ruby_block "create Habitat project release: #{new_resource.name} #{build_version}" do
+    block do
+      # This helper is part of the Delivery Sugar DSL...it's also an alias
+      # for `define_project_application`.
+      create_workflow_application_release(
+        new_resource.name,
+        build_version,
         'artifact' => last_build_env.merge('type' => 'hart'),
-        'delivery_data' => node['delivery'] }
-    }
-  end
-
-  chef_environment get_acceptance_environment do
-    override_attributes lazy {
-      updated_environment_overrides
-    }
+        'delivery_data' => node['delivery']
+      )
+    end
   end
 end
