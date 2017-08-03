7c5426f6c3faa94eb94d2464eb6d40b9f36474dd
Seth Chisamore <schisamo@chef.io>
2017-08-02 20:49:59 -0400

    Remove frivolous load_delivery_chef_config
    This is not needed as `DeliverySugar::DSL.get_project_secrets` already
    loads the Automate instance's Chef Server config when needed.
    
    Besides that, see chef-cookbooks/cia_infra#96 for details on why you should
    never, never, never, never, never, never use `load_delivery_chef_config`!
    
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'habitat-build'
 maintainer 'The Habitat Maintainers'
 maintainer_email 'humans@habitat.sh'
 license 'apache2'
-version '0.14.6'
+version '0.14.7'
 
 depends 'delivery-sugar'
 depends 'delivery-truck'
